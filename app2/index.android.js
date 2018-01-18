087841760bbca07967db8a4be4ef8835b833154d
Diego Augusto <diih_augusto@live.com>
2018-01-18 20:31:40 -0200

    customizando botoes

--- app2/index.android.js
+++ app2/index.android.js
@@ -5,39 +5,42 @@
  */
 
 import React, {Component} from 'react';
-import {AppRegistry, StyleSheet, Text, View} from 'react-native';
+import {AppRegistry, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default class app2 extends Component {
   render() {
     return (
-      <View style={styles.estiloView}>
-        <Text style={styles.estiloTexto}>A</Text>
-        <Text style={styles.estiloTexto}>B</Text>
-        <Text style={styles.estiloTexto2}>C</Text>
+      <View style={styles.principal}>
+        <TouchableOpacity style={styles.botao}>
+          <Text style={styles.textoBotao}>Clique Aqui</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
 
+const botaoPressionado = () =>{
+  alert("Botão pressionado");
+}
 const styles = StyleSheet.create({
-  estiloTexto: {
-    fontSize: 40,
-    backgroundColor: '#08509B',
-    height: 60,
-    width: 60
+  principal: {
+    paddingTop: 40
   },
-  estiloTexto2: {
-    fontSize: 40,
-    backgroundColor: '#2A48fA',
-    height: 60,
-    width: 60
+  botao: {
+    backgroundColor: '#48BBEC',
+    padding: 10,
+    borderColor: "#1d8eb8",
+    borderWidth: 1,
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.4
   },
-  estiloView: {
-    backgroundColor: 'skyblue',
-    height: 300,
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    flexDirection: 'column'
+  textoBotao: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+    alignSelf: 'center'
   }
 });
 
