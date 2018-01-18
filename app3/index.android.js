e07397b639e2025441441a46157ee172cad8113f
Diego Augusto <diih_augusto@live.com>
2018-01-18 21:50:40 -0200

    implementação do props e state

--- app3/index.android.js
+++ app3/index.android.js
@@ -4,50 +4,40 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, {Component} from 'react';
+import {AppRegistry, StyleSheet, Text, View, Button} from 'react-native';
 
-export default class app3 extends Component {
+export class MeuComponente extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+      <View>
+        <Text>{this.props.teste}</Text>
+      </View>
+    )
+  }
+}
+
+export default class app3 extends Component {
+  
+  constructor(props){
+    super(props)
+    this.state = { texto: 'Texto teste 2'};
+  }
+
+  alteraTexto(){
+    this.setState({ texto: 'Outra coisa'});
+  }
+
+  render() {
+    return (
+      <View>
+        <MeuComponente teste={this.state.texto}></MeuComponente>
+        <Button title='Botão' onPress={() => {this.alteraTexto()}}/>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+const styles = StyleSheet.create({});
 
 AppRegistry.registerComponent('app3', () => app3);
