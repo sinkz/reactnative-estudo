75bce83af3f15df87e25cd62343b66075f171cf5
Diego Augusto <diih_augusto@live.com>
2018-01-23 23:06:27 -0200

    Trabalhando com listas e requisições http

--- app4/src/components/ListaItens.js
+++ app4/src/components/ListaItens.js
@@ -1,27 +1,37 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
+import {ScrollView} from 'react-native';
+import axios from 'axios';
+import Itens from './Itens';
 
 export default class ListaItens extends Component {
   constructor(props) {
-    super(props);
-    console.log("Construtor");
+    super(props)
+    this.state = {
+      listaItens: []
+    }
   }
   componentWillMount() {
-    console.log("Antes de renderizar");
-  }
-
-  componentDidMount() {
-    console.log("Depois de renderizar");
+    //req http
+    axios
+      .get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
+      .then(response => {
+        this.setState({listaItens: response.data})
+      })
+      .catch(() => {
+        console.log('Erro ao recuperar os dados')
+      });
   }
 
   render() {
-    console.log("Objeto renderizado");
     return (
-      <View>
-        <Itens/>
-        <Itens/>
-        <Itens/>
-      </View>
+      <ScrollView style={{
+        backgroundColor: '#DDD'
+      }}>
+        {this
+          .state
+          .listaItens
+          .map((item) => (<Itens key={item.titulo} item={item}/>))}
+      </ScrollView>
     );
   }
 }
